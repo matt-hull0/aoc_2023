3eac5fb7b386ddbb7d9d24095590d6411af083f0
Matthew.Hull <matthew.hull@shell.com>
2023-12-13 02:38:37 +0000

    d12 b4 memoization

--- aoc_2023/day_12.py
+++ aoc_2023/day_12.py
@@ -1,10 +1,13 @@
 import re
+import itertools
+from collections import Counter
 
 
 def arrangement_compatible(string1, string2) -> bool:
     # string 1 must be the record
     assert len(string1) == len(string2)
     if string2 in checked:
+        raise ValueError("STILL CHECKING DUPS")
         return False
     for x, y in zip(string1, string2):
         if x == "#" and y != "#":
@@ -24,13 +27,34 @@ def add_dot(seed, loc) -> str:
         return seed[0 : match.end()] + "." + seed[match.end() :]
 
 
+def find_indices_to_add_dots(seed) -> tuple:
+    matches = list(re.finditer(r"(#+)", seed))
+
+    indices = (0, *(match.end() for match in matches))
+
+    return indices
+
+
 def calc_arrangements(record, seed, num_places_to_add_dots) -> int:
     if len(record) == len(seed):
         return arrangement_compatible(record, seed)
     else:
+        num_dots_to_add = len(record) - len(seed)
         extra_arrangements = 0
-        for loc in range(num_places_to_add_dots):
-            new_seed = add_dot(seed, loc)
+
+        ind_to_add_dots = find_indices_to_add_dots(seed)
+        for x in itertools.combinations_with_replacement(
+            ind_to_add_dots, num_dots_to_add
+        ):
+            # print(x)
+            ind_dot_counts = Counter(x)
+            # print(ind_dot_counts)
+            new_seed = seed
+            for ind, num_dots in sorted(
+                list(ind_dot_counts.items()), key=lambda x: x[0], reverse=True
+            ):
+                new_seed = new_seed[:ind] + "." * num_dots + new_seed[ind:]
+
             extra_arrangements += calc_arrangements(
                 record, new_seed, num_places_to_add_dots
             )
@@ -43,25 +67,55 @@ with open("aoc_2023\\day_12.txt") as f:
 
 ans = 0
 
-for record, check in lines:
-    seed = ".".join([int(x) * "#" for x in check.split(",")])
-    # print(seed)
-    checked = set()
-    num_places_to_add_dots = len(check.split(",")) + 1
+# for i, (record, check) in enumerate(lines, 1):
+#     print(f"Line {i}")
+#     seed = ".".join([int(x) * "#" for x in check.split(",")])
+#     start_dots = re.findall(r"(^\.+)", record)
+#     if start_dots:
+#         seed = start_dots[0] + seed
+#     end_dots = re.findall(r"(\.+$)", record)
+#     if end_dots:
+#         seed = seed + end_dots[0]
+#     # print(seed)
+#     checked = set()
+#     num_places_to_add_dots = len(check.split(",")) + 1
 
-    ans += calc_arrangements(record, seed, num_places_to_add_dots)
+#     extra = calc_arrangements(record, seed, num_places_to_add_dots)
+#     print(extra)
+#     ans += extra
 
 print(f"Part 1: {ans=}")
 
 
 # Part 2
-
-for record, check in lines:
-    record = record * 5
-    check = check * 5
+for i, (record, check) in enumerate(lines, 1):
+    print(f"Line {i}")
+    record = "?".join([record for _ in range(5)])
+    check = ",".join([check for _ in range(5)])
     seed = ".".join([int(x) * "#" for x in check.split(",")])
+    start_dots = re.findall(r"(^\.+)", record)
+    if start_dots:
+        seed = start_dots[0] + seed
+    end_dots = re.findall(r"(\.+$)", record)
+    if end_dots:
+        seed = seed + end_dots[0]
     # print(seed)
     checked = set()
     num_places_to_add_dots = len(check.split(",")) + 1
 
-    ans += calc_arrangements(record, seed, num_places_to_add_dots)
+    extra = calc_arrangements(record, seed, num_places_to_add_dots)
+    print(extra)
+    ans += extra
+
+print(f"Part 1: {ans=}")
+
+# old
+# for record, check in lines:
+#     record = record * 5
+#     check = check * 5
+#     seed = ".".join([int(x) * "#" for x in check.split(",")])
+#     # print(seed)
+#     checked = set()
+#     num_places_to_add_dots = len(check.split(",")) + 1
+
+#     ans += calc_arrangements(record, seed, num_places_to_add_dots)
--- aoc_2023/day_12_attempt_2.py
+++ aoc_2023/day_12_attempt_2.py
@@ -23,27 +23,57 @@ def make_as_many_separate_springs_as_poss(arrange):
     return "".join(new_list)
 
 
+def calc_set_so_far(arr_string):
+    q_ind = arr_string.find("?")
+    arr_string = arr_string[0:q_ind]
+
+    dot_ind = arr_string.rfind(".")
+    arr_string = arr_string[0:dot_ind] if dot_ind != -1 else ""
+    return arr_string
+
+
 def arrangement_compatible(record, arrange, check) -> bool:
     # string 1 must be the record
     # up_to_hash_q = re.search(r".+?(?=#+\?)", arrange).group(0)
     # set_so_far_match = re.search(r"^[#.]+?(?=#+\?)", arrange)
-    set_so_far_match = re.search(r"^[#.]+?(?=(#+\?|\.\?))", arrange)
-    up_to_q = set_so_far_match.group(0) if set_so_far_match else ""
+
+    # check what is fixed so far matches the check
+    # set_so_far_match = re.search(r"^[^#][#.]*?(?=(#+\?|\.\?))", arrange)
+    # up_to_q = set_so_far_match.group(0) if set_so_far_match else ""
+    up_to_q = calc_set_so_far(arrange)
     subset_check = [x.end() - x.start() for x in re.finditer(r"#+", up_to_q)]
     if subset_check != check[: len(subset_check)]:
         return False
+
+    # check not too many # in next block of "undecided" ###??
+    springs_up_to_undetermined = re.search(r"^[\.#]+[^\?]", arrange)
+    springs_up_to_undetermined = (
+        springs_up_to_undetermined.group(0) if springs_up_to_undetermined else ""
+    )
+    subset_check = [
+        x.end() - x.start() for x in re.finditer(r"#+", springs_up_to_undetermined)
+    ]
+
+    if len(subset_check) > len(check):
+        return False
+    if subset_check and subset_check[-1] > check[len(subset_check) - 1]:
+        return False
+
+    # check possible to generate enough #
     # first of record [?or#] must be >= first of check?
     possible_longest_springs = [
         x.end() - x.start() for x in re.finditer(r"[\?#]+", arrange)
     ]
-    # if len(possbile_springs) < len(check): NOT VALID STATEMENT
-    #     return False
 
     if sum(possible_longest_springs) < sum(check):
         return False
+
+    #
     left = Counter(possible_longest_springs) - Counter(check)
     if any([v < 0 for v in left.values()]):
         return False
+
+    # check possible to make the number of distinct group of springs required
     as_many_separate_springs_as_possible = make_as_many_separate_springs_as_poss(
         arrange
     )
@@ -86,17 +116,55 @@ def calculate(record, arrange, check):
         return False
 
 
+record = "?".join(["???.###" for _ in range(5)])
+check = "1,1,3"
+check = ",".join([check for _ in range(5)])
+check = list(map(int, check.split(",")))
+arrange = record
+# assert calculate(record, arrange, check) == 1
+
+record = "?".join([".??..??...?##." for _ in range(5)])
+check = ",".join(["1,1,3" for _ in range(5)])
+check = list(map(int, check.split(",")))
+arrange = record
+# assert calculate(record, arrange, check) == 16384
+
+record = "?".join(["?#?#?#?#?#?#?#?" for _ in range(5)])
+check = ",".join(["1,3,1,6" for _ in range(5)])
+check = list(map(int, check.split(",")))
+arrange = record
+# assert calculate(record, arrange, check) == 1
+
+record = "?".join(["????.#...#..." for _ in range(5)])
+check = ",".join(["4,1,1" for _ in range(5)])
+check = list(map(int, check.split(",")))
+arrange = record
+# assert calculate(record, arrange, check) == 16
+
+record = "?".join(["????.######..#####." for _ in range(5)])
+check = ",".join(["1,6,5" for _ in range(5)])
+check = list(map(int, check.split(",")))
+arrange = record
+# assert calculate(record, arrange, check) == 2500
+
+record = "?".join(["?###????????" for _ in range(5)])
+check = ",".join(["3,2,1" for _ in range(5)])
+check = list(map(int, check.split(",")))
+arrange = record
+# assert calculate(record, arrange, check) == 506250
+
+
 with open("aoc_2023\\day_12.txt") as f:
     lines = [line.strip().split(" ") for line in f.readlines()]
 
 
 ans = 0
 
-# for i, (record, check) in enumerate(lines, 1):
-#     arrange = record
-#     check = list(map(int, check.split(",")))
-#     checked = set()
-#     ans += calculate(record, arrange, check)
+for i, (record, check) in enumerate(lines, 1):
+    arrange = record
+    check = list(map(int, check.split(",")))
+    checked = set()
+    ans += calculate(record, arrange, check)
 
 print(f"Part 1: {ans=}")
 
